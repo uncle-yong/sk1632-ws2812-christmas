c3247d7c6452b01db3e3ad7a7e244dc16c0581db
Uncle Yong's Workshop <The_YongGrand@yahoo.com>
2017-11-19 21:37:14 +0800

    First upload
    First upload.

---
+++ firmware/sk1632-ws2812-christmas.X/build/default/production/_ext/1360937237/light_patterns.o.d
@@ -0,0 +1,6 @@
+build/default/production/_ext/1360937237/light_patterns.o:  \
+ ../src/light_patterns.c  \
+../src/main.h  \
+../src/ws2812_pic32mx.h  \
+ ../../../../framework/system/common/sys_module.h  \
+ ../../../../framework/system/common/sys_common.h 
---
+++ firmware/sk1632-ws2812-christmas.X/build/default/production/_ext/1360937237/ws2812_pic32mx.o.d
@@ -0,0 +1,6 @@
+build/default/production/_ext/1360937237/ws2812_pic32mx.o:  \
+ ../src/ws2812_pic32mx.c  \
+../src/main.h  \
+../src/ws2812_pic32mx.h  \
+ ../../../../framework/system/common/sys_module.h  \
+ ../../../../framework/system/common/sys_common.h 
